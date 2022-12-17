04be9ba8700c6a6e7bf8b89b368588094bbe9dd5
anna <annagridchina@mail.ru>
2022-12-17 19:16:13 +0700

    работает

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("start");
+
+int N = 10;
+
+for (int i = 2; i < N + 1; i = i + 2)
+
+Console.WriteLine(i);
+
+Console.WriteLine("end");
